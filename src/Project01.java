375492b67621cf84779ae9987c1fded369ded422
Tharindu Deshananda <tdeshananda@gmail.com>
2019-11-01 10:29:10 +0530

    third commit commit

--- src/Project01.java
+++ src/Project01.java
@@ -1,8 +1,14 @@
-public class Project01 {
+import java.util.Scanner;
+
+ class Project01 {
 
 public static void main(String args[]){
-    System.out.println("Hello World");
-    System.out.println("Tharindu Deshananda");
+    System.out.println("WELCOME to University Management System");
+    int num=0;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Press [1] for Student Registration\nPress [2] for Lecture Registration");
+    num=sc.nextInt();
+    
 
 }
 
--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,6 @@
 public class Student {
+    int num;
+    String name;
+    Student(){num=0;}
+    Student(int x,String str){num=x; name=str;}
 }
