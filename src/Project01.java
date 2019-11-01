fbd4d881cd8c2ff03bcf4a8b81ef962dc4a17aed
Tharindu Deshananda <tdeshananda@gmail.com>
2019-11-01 10:33:19 +0530

    fourth commit commit

--- src/Project01.java
+++ src/Project01.java
@@ -8,8 +8,9 @@ public static void main(String args[]){
     Scanner sc=new Scanner(System.in);
     System.out.println("Press [1] for Student Registration\nPress [2] for Lecture Registration");
     num=sc.nextInt();
-    
-
+    if(num==1){System.out.println("Welcome to Student Registration");}
+    else if(num==2){System.out.println("Welcome to Lecture Registration");}
+    else{System.out.println("Enter valid number");}
 }
 
 }
